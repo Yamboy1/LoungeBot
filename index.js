22cb1d1a5c3ac4e19d0af0e44bb0ac2b4f23e478
Yamboy1 <16280@grammar.net.nz>
2019-01-30 20:11:45 +1300

    Remove all implementations, start anew with example index

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const Discord = require("discord.js");
+const { Client } = require("discord.js");
+
+const Loader = require("./Loader");
 
 const { token } = require("./secrets.json");
 
+const client = new Client();
+const loader = new Loader("./Commands");
 
+client.on("message", message => {
+    const commandName = message.content.toLowerCase();
+    if (loader.has(commandName)) {
+        loader.get(commandName).execute({ client, message })
+    }
+}
+
+client.login(token);
