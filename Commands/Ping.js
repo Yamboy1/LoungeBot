bcbca0ed2ab9b455f4031881cfa044e32e37f002
Yamboy1 <16280@grammar.net.nz>
2019-01-31 17:53:59 +1300

    Create ping command

---
+++ Commands/Ping.js
@@ -0,0 +1,16 @@
+const Command = require("../Command");
+
+class PingCommand extends Command {
+    constructor() {
+        super({
+            name: "ping",
+            description: "This commands tells you your ping."
+        });
+    }
+    execute({ client, message }) {
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);  
+    }
+}
+
+module.exports = PingCommand;
