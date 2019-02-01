3f4c123bd74b819cc3731bb4aebb21a39056a34c
Yamboy1 <16280@grammar.net.nz>
2019-02-01 20:50:52 +1300

    update ping command to support d.js master

--- Commands/Ping.js
+++ Commands/Ping.js
@@ -9,7 +9,7 @@ class PingCommand extends Command {
     }
     async execute({ client, message }) {
         const m = await message.channel.send("Ping?");
-        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);  
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
 }
 
