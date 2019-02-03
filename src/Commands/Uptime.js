433c2fa7999b74ebfd0e71bbbcde4eb70aeec917
Yamboy1 <16280@grammar.net.nz>
2019-02-03 20:47:34 +1300

    Add uptime command

---
+++ src/Commands/Uptime.js
@@ -0,0 +1,18 @@
+const moment = require("moment");
+require("moment-precise-range-plugin");
+
+const { Command } = require("../Structures");
+
+class Uptime extends Command {
+    constructor() {
+        super({
+            name: "uptime",
+            description: "This command tells you my uptime"
+        });
+    }
+    async execute({ message }) {
+        await message.channel.send(`My uptime is \`${moment.preciseDiff(client.uptime, moment.now)}\``)
+    }
+}
+
+module.exports = Uptime;
