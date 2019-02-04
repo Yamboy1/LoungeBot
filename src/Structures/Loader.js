9a3311cd5b98479e1c1bcdada6e07c27673b91ed
Yamboy1 <16280@grammar.net.nz>
2019-02-04 21:02:27 +1300

    fix bug ( i really need to start testing before i push)

--- src/Structures/Loader.js
+++ src/Structures/Loader.js
@@ -3,7 +3,7 @@ const { Collection } = require("discord.js");
 class Loader extends Collection {
     constructor(filenames, cwd=null) {
         super();
-        if (this.filenames) {
+        if (filenames) {
             this.filenames = filenames;
             this.cwd = cwd;
             this.load();
